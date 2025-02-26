2ce94b5d2234c5414340470af98e53799d99b3c1
OMKAR9395 <omkarmohite85@gmail.com>
2025-02-26 11:14:23 +0530

    Dart

---
+++ DataTypesAndVariables/09_Dynamic_Data_Type.dart
@@ -0,0 +1,14 @@
+void main(){
+  dynamic Number = 10;
+  print(Number);
+  print(Number.runtimeType);
+
+  Number = "WebX";
+    print(Number);
+  print(Number.runtimeType);
+
+  Number = true;
+
+    print(Number);
+  print(Number.runtimeType);
+}
