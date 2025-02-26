93845ab53bcc9023e9747616ad7777235ec5c2c0
OMKAR9395 <omkarmohite85@gmail.com>
2025-02-26 11:11:15 +0530

    Dart Data Types

---
+++ DataTypesAndVariables/07_Num_Data_Type.dart
@@ -0,0 +1,10 @@
+void main(){
+  num x = 10;
+
+  print(x);
+  print(x.runtimeType);
+
+  x=10.201;
+  print(x);
+  print(x.runtimeType);
+}
---
+++ DataTypesAndVariables/08_Var_Data_Type.dart
@@ -0,0 +1,6 @@
+void main(){
+  var x = 20;
+  print(x);
+  print(x.runtimeType);
+  //  x=20.10;       Cannot Change int to float data type
+}
