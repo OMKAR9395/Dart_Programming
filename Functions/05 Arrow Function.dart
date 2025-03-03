79aa6e4785fda0a3aeccead2fb190097a0a615ae
OMKAR9395 <omkarmohite85@gmail.com>
2025-03-03 16:59:11 +0530

    Arrow Function

--- Functions/05 Arrow Function.dart
+++ Functions/05 Arrow Function.dart
@@ -1,4 +1,4 @@
-int multiply(int a, int b) => a * b;
+int multiply(int a, int b) => a * b;          //No Return Type Syntax
 
 void main() {
   print("Multiplication of Two Numbers: ${multiply(5, 4)}");
