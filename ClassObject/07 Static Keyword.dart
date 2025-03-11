e4dafdf697551a929999c091f21aee8c06bff028
OMKAR9395 <omkarmohite85@gmail.com>
2025-03-11 11:38:07 +0530

    static

--- ClassObject/07 Static Keyword.dart
+++ ClassObject/07 Static Keyword.dart
@@ -1,23 +1,51 @@
 class Car {
-  static int totalCars = 0;
+  static int totalCars = 10;     //Static variable
   String brand;
 
-  Car(this.brand) 
+ 
+  Car(this.brand)   // Constructor Parameterized     
   {
-    totalCars++;
-  }
+    print(this.brand);
+    
+    int brand = 10;
+    print('We Are Inside Of Constructor ${this.brand}');
 
-  static void showTotalCars() 
+    print('We Are Inside Of Constructor $brand');
+  }
+  // Car(int a ,int b)   // Constructor Two Parameterized      
+  // {
+  //   print(this.brand);
+    
+  //   int brand = 10;
+  //   print('We Are Inside Of Constructor ${this.brand}');
+
+  //   print('We Are Inside Of Constructor $brand');
+  // }
+
+  // Car(){                        // Default Constructor
+  //   print("Default Constructor");
+  // }
+
+  static void showTotalCars() //static Function
   {
     print("Total Cars: $totalCars");
   }
+   
 }
 
 void main() 
 {
-  Car car1 = Car("Tesla");
-  Car car2 = Car("BMW");
+  Car car1 = new Car("Tesla");     // Single Parameterized object
+  Car car2 = new Car("BMW");       // Single Parameterized object
+  // Car car3 = new Car ();           // Default object created
+  // Car Car4 = new Car(10,20);       // Two Parameterizes Objecy
 
-  Car.showTotalCars(); 
-  print(Car.totalCars);
+  print('We Are Inside Of Main ${car1.brand}');
+  print('We Are Inside Of Main ${car2.brand}');
+
+  // car1.showTotalCars();        //Cannot Acces  with object because ShowTotalCars function is Static
+  // car2.showTotalCars();        //Cannot Acces  with object because ShowTotalCars function is Static
+
+  Car.showTotalCars();            // Access With Class Name Static Functions And Variables
+  print(Car.totalCars);          // Access With Class Name Static Functions And Variables 
 }
