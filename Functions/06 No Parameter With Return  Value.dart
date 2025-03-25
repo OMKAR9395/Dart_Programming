3b5bde7d31af28751a0c6ea70a022ee7bfc54d2a
OMKAR9395 <omkarmohite85@gmail.com>
2025-03-25 12:53:42 +0530

    Parameter

---
+++ Functions/06 No Parameter With Return  Value.dart
@@ -0,0 +1,9 @@
+int add(){    /// Function Defination
+  int No1 = 40;   ///   Variable Initialization
+  int No2 = 70;   ///   Variable Initialization
+  return No1 + No2;       ///   Return Addition
+}
+void main(){    ///   Entry Point Function
+  int Ret =  add();       /// Variable initialization And Function Calling
+  print(Ret);   ///   Print Value Return
+}
