680a9a6c4acdcde9954d9208c88de6bfbbdd8905
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-04 22:52:58 +0530

    constructor

--- Functions/Optional/01 Positional Parameter.dart
+++ Functions/Optional/01 Positional Parameter.dart
@@ -1,8 +1,9 @@
 void greet(String name, int age) {    ///   parameterized function defination
+
     print("Hello, my name is $name and I am $age years old.");      ///print
   }
   void main() {   /// entry point function
-  
+
     greet("Omkar", 24); ///Function Calling
   }
   
