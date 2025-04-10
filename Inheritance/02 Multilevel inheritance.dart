a762e5928e6fe1392f18979f0079a853394bc42c
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-10 20:50:46 +0530

    Assignment

---
+++ Inheritance/02 Multilevel inheritance.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+
+class Laptop {      /// Parent Class
+  String? brand;
+  String? processor;
+
+  Laptop() {    /// Parent Constructor
+    stdout.writeln('================= Enter Laptop Details ==================');
+  }
+
+  void getDetails() {     /// Parent Method
+    stdout.writeln('Enter Laptop Brand Name:');
+    brand = stdin.readLineSync();
+
+    stdout.writeln('Enter Laptop Processor Name:');
+    processor = stdin.readLineSync();
+  }
+}
+
+class Keyboard extends Laptop {     /// Child Class of Laptop And Parent Class of  Display
+  String? keyBoard;
+  String? backlight;
+
+  Keyboard() {               /// Parent Constructor
+    stdout.writeln('\n================ Enter Keyboard Details ================\n');
+  }
+
+  @override
+  void getDetails() {     /// Parent Method
+    super.getDetails();
+
+    stdout.writeln('Enter Keyboard Type (Gaming / Normal):');
+    keyBoard = stdin.readLineSync();
+
+    stdout.writeln('Enter Backlight Type (RGB / White / Pink):');
+    backlight = stdin.readLineSync();
+  }
+}
+
+class Display extends Keyboard {    /// Child Class of Display
+  String? screenSize;
+  String? displayType;
+
+  Display() {
+    stdout.writeln('\n================ Enter Display Details ================\n');
+  }
+
+  @override
+  void getDetails() {
+    super.getDetails();
+
+    stdout.writeln('Enter Screen Size (14 inch, 15.6 inch):');
+    screenSize = stdin.readLineSync();
+
+    stdout.writeln('Enter Display Type (LED / OLED / LCD):');
+    displayType = stdin.readLineSync();
+  }
+
+  void showDetails() {
+    stdout.writeln('\n================ Full Laptop Details Here ================');
+    stdout.writeln('Brand           : $brand');
+    stdout.writeln('Processor       : $processor');
+    stdout.writeln('Keyboard Type   : $keyBoard');
+    stdout.writeln('Backlight       : $backlight');
+    stdout.writeln('Screen Size     : $screenSize');
+    stdout.writeln('Display Type    : $displayType');
+  }
+}
+void main() {
+  Display obj = new Display();
+  obj.getDetails();
+  obj.showDetails();
+}
