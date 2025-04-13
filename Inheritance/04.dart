2863c67fc7ff6a6bbca36364b2bfbea29c188593
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-13 23:22:57 +0530

    Assignment

--- Inheritance/04.dart
+++ Inheritance/04.dart
@@ -6,6 +6,7 @@ void main() {
 int add(String a, String b) {
   int retVal = 0;
   try {
+    
     retVal = int.parse(a) + int.parse(b);
   } catch (e) {
     retVal = int.parse(a) + int.parse("0");
