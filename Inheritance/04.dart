e08b6c2e0ffb8f55cef65eeb47e98c7bfc973687
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-13 23:21:31 +0530

    Assignment

---
+++ Inheritance/04.dart
@@ -0,0 +1,14 @@
+void main() {
+    print(add("10","-"));
+    print('Done...');
+}
+int add(String a, String b) {
+  int retVal = 0;
+  try {
+    retVal = int.parse(a) + int.parse(b);
+  } catch (e) {
+    retVal = int.parse(a) + int.parse("0");
+    print("Exception in add() : $e");
+  }
+  return retVal;
+}
