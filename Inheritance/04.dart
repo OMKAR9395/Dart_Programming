4d79a484e25bc9e9eb8fdf008881049a18793144
OMKAR9395 <omkarmohite85@gmail.com>
2025-04-18 23:07:05 +0530

    a

--- Inheritance/04.dart
+++ Inheritance/04.dart
@@ -5,11 +5,12 @@ void main() {
 int add(String a, String b) {
   int retVal = 0;
   try { 
-    
+
     retVal = int.parse(a) + int.parse(b);
   } catch (e) {
     retVal = int.parse(a) + int.parse("0");  
     print("Exception in add() : $e");
   }
+  
   return retVal;
 }
