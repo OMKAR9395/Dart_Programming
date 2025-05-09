121092f43de26ed4a7b96c2279201deca7a3bd08
OMKAR9395 <omkarmohite85@gmail.com>
2025-05-09 23:28:45 +0530

    Assignment

--- Recursion/01WithStaticAssignment.dart
+++ Recursion/01WithStaticAssignment.dart
@@ -1,9 +1,10 @@
 class Fact {
-  static int callCount = 0; // Static variable to count recursive calls
+  static int callCount = 0;           /// Static variable to count recursive calls
 
-  static int factorial(int n) {
-    callCount++; // Increments on every call
-    if (n == 0 || n == 1) {
+  static int factorial(int n) {       /// Static Method
+    callCount++;       /// Increments on every call
+
+    if (n == 0 || n == 1) {         /// Condition Check
       return 1;
     } else {
       return n * factorial(n - 1);
@@ -11,9 +12,9 @@ class Fact {
   }
 }
 
-void main() {
-  int number = 5;
-  int result = Fact.factorial(number);
+void main() {           ///Entry point Fuunction
+  int number = 5;         /// variable
+  int result = Fact.factorial(number);   ///Calling method factorial and assigning result variable
   
   print("Factorial of $number is $result");
   print("Recursive calls made: ${Fact.callCount}");
