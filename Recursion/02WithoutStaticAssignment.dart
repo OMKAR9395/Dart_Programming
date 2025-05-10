c41d1dae654b0ac914b501c23a3d3d59a46085c6
OMKAR9395 <omkarmohite85@gmail.com>
2025-05-10 23:17:02 +0530

    Assignment

--- Recursion/02WithoutStaticAssignment.dart
+++ Recursion/02WithoutStaticAssignment.dart
@@ -5,7 +5,6 @@ class FactorialCalculator {
     return (n <= 1) ? 1 : n * factorial(n-1);         ///using turnery Operator And Condition if else
   }
 }
-
 void main() {     ///entry point function
 
   stdout.write("Enter a number: ");                 ///user input valuue
